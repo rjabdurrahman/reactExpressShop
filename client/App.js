8ed6831f900e59981b8477d0f3211fd85ff879e3
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-02-11 12:40:42 -0600

    Added: local storage cart

--- client/App.js
+++ client/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import MainRouter from './MainRouter'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@material-ui/styles'
 import theme from './theme'
 import { hot } from 'react-hot-loader'
 
 const App = () => {
+  localStorage.setItem('cart', JSON.stringify([
+    {
+      product: "Product 1",
+      quantity: 1,
+      shop: '14252525'
+    }
+  ]))
   React.useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side')
     if (jssStyles) {
@@ -13,11 +20,11 @@ const App = () => {
     }
   }, [])
   return (
-  <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <MainRouter/>
-      </ThemeProvider>
-  </BrowserRouter>
+    < BrowserRouter >
+    <ThemeProvider theme={theme}>
+      <MainRouter />
+    </ThemeProvider>
+  </BrowserRouter >
 )}
 
 export default hot(module)(App)
